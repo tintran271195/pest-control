5f7858ea91fa2410919c16b95ce06759910acd66
tintran271195@gmail.com <tintran271195@gmail.com>
2021-08-23 10:35:43 +0700

    first commit

---
+++ src/main/java/com/example/pestcontrol/PestControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.pestcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PestControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PestControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pestcontrol/PestControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pestcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PestControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
